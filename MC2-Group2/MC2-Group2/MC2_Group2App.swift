fd56b3c7a5c16a856bbe8412ff94fefe304a17a7
Nadia Ramadhani <103446529+twicetw@users.noreply.github.com>
2022-06-19 08:11:54 +0700

    New file

---
+++ MC2-Group2/MC2-Group2/MC2_Group2App.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_Group2App.swift
+//  MC2-Group2
+//
+//  Created by Local Administrator on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MC2_Group2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
